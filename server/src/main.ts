e2a192cba6eabbe13f85afb62b630e1cd36ae53c
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-12-06 19:56:21 -0300

    add Swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerModule,
+} from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.setGlobalPrefix('api/v1')
+
+  //
+  // Swagger
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Floorplan API')
+    .setVersion('1.0')
+    .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig)
+
+  const customOptions: SwaggerCustomOptions = {
+    customSiteTitle: 'Floorplan API',
+  }
+
+  SwaggerModule.setup('docs', app, documentFactory, customOptions)
+
+  //
+  // Listen
+
   await app.listen(3000)
 }
+
 bootstrap()

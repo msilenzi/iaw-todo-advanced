13abe0eed931b1b4925947231f53734c7226c0f0
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-12-07 21:49:06 -0300

    add Mantine

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,8 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+import '@mantine/core/styles.css'
+import { MantineProvider } from '@mantine/core'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MantineProvider defaultColorScheme="dark">
+      <App />
+    </MantineProvider>
   </StrictMode>,
 )

219a81831cabc680cbe7456114e3e98cdea99af3
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-12-17 12:57:55 -0300

    signup is working

---
+++ client/src/features/auth/hooks/useSignup.ts
@@ -0,0 +1,122 @@
+import authApi from '@Common/api/auth.api'
+import { SignUpDtoGenderEnum } from '@Common/api/generated'
+import useNotifications from '@Common/hooks/useNotifications'
+import { useForm } from '@mantine/form'
+import { useNavigate } from '@tanstack/react-router'
+import { isAxiosError } from 'axios'
+import { useState } from 'react'
+
+export type SignupForm = {
+  firstName: string
+  lastName: string
+  dateOfBirth: Date | null
+  gender: SignUpDtoGenderEnum | ''
+  email: string
+  password: string
+  confirmPassword: string
+}
+
+export default function useSignup() {
+  const navigate = useNavigate()
+  const { showSuccessNotification, showErrorNotification } = useNotifications()
+  const [isLoading, setIsLoading] = useState(false)
+
+  const form = useForm<SignupForm>({
+    mode: 'uncontrolled',
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      dateOfBirth: null,
+      gender: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+    validate: {
+      firstName: (value) =>
+        value.trim().length === 0 ? 'El nombre es obligatorio' : null,
+      lastName: (value) =>
+        value.trim().length === 0 ? 'El apellido es obligatorio' : null,
+      dateOfBirth: (value) =>
+        value === null ? 'La fecha de nacimiento es obligatoria' : null,
+      gender: (value) => (value === '' ? 'El género es obligatorio' : null),
+      email: (value) =>
+        value.trim().length === 0 ? 'El correo es obligatorio'
+        : !/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/.test(value.trim()) ?
+          'El correo es incorrecto'
+        : null,
+      password: (value) =>
+        value.trim().length === 0 ? 'La contraseña es obligatoria'
+        : value.trim().length < 6 ?
+          'La contraseña debe tener al menos 6 caracteres'
+        : null,
+      confirmPassword: (value, values) =>
+        value.trim().length === 0 ?
+          'La confirmación de contraseña es obligatoria'
+        : value !== values.password ? 'Las contraseñas no coinciden'
+        : null,
+    },
+    transformValues: (values) => ({
+      firstName: values.firstName.trim(),
+      lastName: values.lastName.trim(),
+      dateOfBirth: values.dateOfBirth,
+      gender: values.gender,
+      email: values.email.trim(),
+      password: values.password.trim(),
+      confirmPassword: values.confirmPassword.trim(),
+    }),
+  })
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    form.onSubmit(async (values) => {
+      setIsLoading(true)
+      if (values.dateOfBirth == null || values.gender === '') {
+        return
+      }
+
+      try {
+        const resp = await authApi.signUp({
+          firstName: values.firstName,
+          lastName: values.lastName,
+          dateOfBirth: values.dateOfBirth.toISOString(),
+          gender: values.gender,
+          email: values.email,
+          password: values.password,
+        })
+        console.log(resp)
+        showSuccessNotification({
+          title: 'Cuenta creada con éxito',
+          message: 'Ya puedes iniciar sesión con tu cuenta',
+        })
+        void navigate({ to: '/auth/login' })
+      } catch (error) {
+        if (isAxiosError<Error>(error) && error.response) {
+          const { status, data } = error.response
+          if (
+            status === 400 &&
+            data.message === 'Ya existe un usuario con este correo.'
+          ) {
+            form.setErrors({ email: 'Ya existe una cuenta con este correo' })
+          } else {
+            // Otros errores de servidor
+            showErrorNotification({
+              title: 'Ocurrió un error',
+              message:
+                'Hubo un problema al procesar tu solicitud y no pudimos crear tu cuenta. Por favor, inténtalo de nuevo maś tarde',
+            })
+          }
+        } else {
+          showErrorNotification({
+            title: 'Error de conexión',
+            message:
+              'No pudimos conectarnos con el servidor. Por favor, verifica tu conexión.',
+          })
+        }
+      } finally {
+        setIsLoading(false)
+      }
+    })(e)
+  }
+
+  return { form, isLoading, handleSubmit }
+}
--- client/src/routes/auth/_layout/signup.tsx
+++ client/src/routes/auth/_layout/signup.tsx
@@ -1,3 +1,4 @@
+import useSignup from '@/auth/hooks/useSignup'
 import { SignUpDtoGenderEnum } from '@Common/api/generated'
 import {
   Button,
@@ -9,149 +10,109 @@ import {
   Title,
 } from '@mantine/core'
 import { DatePickerInput } from '@mantine/dates'
-import { useForm } from '@mantine/form'
 import { createFileRoute, Link } from '@tanstack/react-router'
-
-type SignupForm = {
-  firstName: string
-  lastName: string
-  dateOfBirth: Date | null
-  gender: SignUpDtoGenderEnum | ''
-  email: string
-  password: string
-  confirmPassword: string
-}
 
 export const Route = createFileRoute('/auth/_layout/signup')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  const form = useForm<SignupForm>({
-    mode: 'uncontrolled',
-    initialValues: {
-      firstName: '',
-      lastName: '',
-      dateOfBirth: null,
-      gender: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
-    },
-    validate: {
-      firstName: (value) =>
-        value.trim().length === 0 ? 'El nombre es obligatorio' : null,
-      lastName: (value) =>
-        value.trim().length === 0 ? 'El apellido es obligatorio' : null,
-      dateOfBirth: (value) =>
-        value === null ? 'La fecha de nacimiento es obligatoria' : null,
-      gender: (value) => (value === '' ? 'El género es obligatorio' : null),
-      email: (value) =>
-        value.trim().length === 0 ? 'El correo es obligatorio'
-        : !/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/.test(value.trim()) ?
-          'El correo es incorrecto'
-        : null,
-      password: (value) =>
-        value.trim().length === 0 ? 'La contraseña es obligatoria'
-        : value.trim().length < 6 ?
-          'La contraseña debe tener al menos 6 caracteres'
-        : null,
-      confirmPassword: (value, values) =>
-        value.trim().length === 0 ?
-          'La confirmación de contraseña es obligatoria'
-        : value !== values.password ? 'Las contraseñas no coinciden'
-        : null,
-    },
-    transformValues: (values) => ({
-      firstName: values.firstName.trim(),
-      lastName: values.lastName.trim(),
-      dateOfBirth: values.dateOfBirth,
-      gender: values.gender,
-      email: values.email.trim(),
-      password: values.password.trim(),
-      confirmPassword: values.confirmPassword.trim(),
-    }),
-  })
+  const { form, isLoading, handleSubmit } = useSignup()
 
   return (
     <>
       <Title ta="center" order={2} mb="md">
         Crear una cuenta
       </Title>
-      <form onSubmit={form.onSubmit((values) => console.log(values))}>
-        <Stack>
-          <TextInput
-            label="Nombre"
-            placeholder="Nombre"
-            withAsterisk
-            autoFocus
-            key={form.key('firstName')}
-            {...form.getInputProps('firstName')}
-          />
-          <TextInput
-            label="Apellido"
-            placeholder="Apellido"
-            withAsterisk
-            key={form.key('lastName')}
-            {...form.getInputProps('lastName')}
-          />
-          <DatePickerInput
-            label="Fecha de nacimiento"
-            placeholder="Fecha de nacimiento"
-            valueFormat="DD/MM/YYYY"
-            withAsterisk
-            key={form.key('dateOfBirth')}
-            {...form.getInputProps('dateOfBirth')}
-          />
-          <NativeSelect
-            label="Género"
-            data={[
-              { label: 'Género', value: '', disabled: true },
-              { label: 'Femenino', value: SignUpDtoGenderEnum.Female },
-              { label: 'Masculino', value: SignUpDtoGenderEnum.Male },
-              { label: 'Otro', value: SignUpDtoGenderEnum.Other },
-            ]}
-            withAsterisk
-            key={form.key('gender')}
-            {...form.getInputProps('gender')}
-          />
-          <TextInput
-            label="Correo"
-            placeholder="Correo"
-            withAsterisk
-            key={form.key('email')}
-            {...form.getInputProps('email')}
-          />
-          <PasswordInput
-            label="Contraseña"
-            placeholder="Contraseña"
-            withAsterisk
-            key={form.key('password')}
-            {...form.getInputProps('password')}
-          />
-          <PasswordInput
-            label="Confirmar contraseña"
-            placeholder="Confirmar contraseña"
-            withAsterisk
-            key={form.key('confirmPassword')}
-            {...form.getInputProps('confirmPassword')}
-          />
 
-          <Button
-            variant="gradient"
-            gradient={{ from: 'blue', to: 'cyan' }}
-            mt="md"
-            type="submit"
-          >
-            Crear cuenta
-          </Button>
-          <Text size="sm" ta="center" mt="xs" c="dimmed">
-            ¿Ya tenés una cuenta?{' '}
-            <Text c="dark.1" td="underline" component={Link} to="/auth/login">
-              Iniciar sesión
+      <form onSubmit={handleSubmit}>
+        <fieldset
+          disabled={isLoading}
+          style={{ margin: 0, padding: 0, border: 'none' }}
+        >
+          <Stack>
+            <TextInput
+              label="Nombre"
+              placeholder="Nombre"
+              withAsterisk
+              autoFocus
+              key={form.key('firstName')}
+              {...form.getInputProps('firstName')}
+            />
+            <TextInput
+              label="Apellido"
+              placeholder="Apellido"
+              withAsterisk
+              key={form.key('lastName')}
+              {...form.getInputProps('lastName')}
+            />
+            <DatePickerInput
+              label="Fecha de nacimiento"
+              placeholder="Fecha de nacimiento"
+              valueFormat="DD/MM/YYYY"
+              withAsterisk
+              key={form.key('dateOfBirth')}
+              {...form.getInputProps('dateOfBirth')}
+            />
+            <NativeSelect
+              label="Género"
+              data={[
+                { label: 'Género', value: '', disabled: true },
+                { label: 'Femenino', value: SignUpDtoGenderEnum.Female },
+                { label: 'Masculino', value: SignUpDtoGenderEnum.Male },
+                { label: 'Otro', value: SignUpDtoGenderEnum.Other },
+              ]}
+              withAsterisk
+              key={form.key('gender')}
+              {...form.getInputProps('gender')}
+            />
+            <TextInput
+              label="Correo"
+              placeholder="Correo"
+              withAsterisk
+              key={form.key('email')}
+              {...form.getInputProps('email')}
+            />
+            <PasswordInput
+              label="Contraseña"
+              placeholder="Contraseña"
+              withAsterisk
+              key={form.key('password')}
+              {...form.getInputProps('password')}
+            />
+            <PasswordInput
+              label="Confirmar contraseña"
+              placeholder="Confirmar contraseña"
+              withAsterisk
+              key={form.key('confirmPassword')}
+              {...form.getInputProps('confirmPassword')}
+            />
+
+            <Button
+              variant="gradient"
+              gradient={{ from: 'blue', to: 'cyan' }}
+              mt="md"
+              type="submit"
+              disabled={isLoading}
+              loading={isLoading}
+              loaderProps={{ type: 'dots' }}
+            >
+              {isLoading ? 'Creando cuenta...' : 'Crear cuenta'}
+            </Button>
+            <Text size="sm" ta="center" mt="xs" c="dimmed">
+              ¿Ya tenés una cuenta?{' '}
+              <Text
+                c="dark.1"
+                td="underline"
+                component={Link}
+                to="/auth/login"
+                disabled={isLoading}
+              >
+                Iniciar sesión
+              </Text>
             </Text>
-          </Text>
-        </Stack>
+          </Stack>
+        </fieldset>
       </form>
     </>
   )
